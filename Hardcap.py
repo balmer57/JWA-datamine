58839e4e0fc153e463bf2520861258ff67166fbf
Balmer <54807083+balmer57@users.noreply.github.com>
2024-08-20 11:48:19 +0300

    Add files via upload

--- Hardcap.py
+++ Hardcap.py
@@ -6,6 +6,14 @@ dna_map = [100, 150, 200, 250, 300, 350, 400, 500, 750, 1000, 1250, 1500, 2000, 
 coin_map = [5, 10, 25, 50, 100, 200, 400, 600, 800, 1000, 2000, 4000, 6000, 8000, 10000, 15000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000, 120000, 150000, 200000, 250000]
 rarity_shift = {"Common": 1, "Rare": 6, "Epic": 11, "Legendary": 16}
 rarity_amount = {"Common": 1, "Rare": 4, "Epic": 15, "Legendary": 50}
+rarity_limits = {}
+
+for cur_dir, dirs, files in os.walk(DATA_DIR + "/Rarity"):
+    for file in files:
+        with open(cur_dir + "/" + file) as f:
+            rarity_json = json.load(f)
+            limits = rarity_json["dl"]
+            rarity_limits[file[1:-5]] = [limits["vis"], limits["inv"]]
 
 with open(BASE_DIR + "/decoded_streams/GetCreatureList.json") as f:
     messages = json.load(f)
@@ -24,11 +32,12 @@ for cur_dir, dirs, files in os.walk(DATA_DIR + "/Resource/Dna"):
             dna_json = json.load(f)
             if dna_json["$type"].startswith("CrDNARe"):
                 if dna_json["l"]:
-                    limit = dna_json["l"]["vis"]
-                    hard_limit = dna_json["l"]["inv"]
+                    # limit = dna_json["l"]["vis"]
+                    # hard_limit = dna_json["l"]["inv"]
                     guid = dna_json["cr"]["guid"]
                     name = find_name(guid)
                     rarity = find_rarity(guid)
+                    limit, hard_limit = rarity_limits[rarity]
                     id = dna_json["TypeId"]
                     level = level_map[guid] if guid in level_map.keys() else 0
                     dna_25 = 0
